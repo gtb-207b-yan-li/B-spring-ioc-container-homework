86532a32bd98b9a132ae7f7d9d9e5bb6ea7e279f
LI Yan <li.yan@thoughworks.com>
2021-11-08 21:25:00 +0800

    [Li Yan] feat: finish problem 3 solution 3

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,15 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Bar {
+public class Bar implements ApplicationContextAware, InitializingBean {
 
     private Foo foo;
-
-    public Bar(Foo foo) {
-        this.foo = foo;
-    }
+    private ApplicationContext context;
 
     public void hi() {
         System.out.println("Hi, " + foo.name());
@@ -17,5 +18,15 @@ public class Bar {
 
     public String name() {
         return "Bar";
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        foo = context.getBean(Foo.class);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
     }
 }
