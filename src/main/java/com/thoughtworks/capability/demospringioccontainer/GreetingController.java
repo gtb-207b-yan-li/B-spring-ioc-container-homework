69832ada9fed1cf490395954cf1aefe8d350f0a7
LI Yan <li.yan@thoughworks.com>
2021-11-08 18:16:39 +0800

    [Li Yan] feat: finish problem 2 solution 2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,23 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class GreetingController implements ApplicationContextAware {
 
-    private final GreetingService greetingService;
-
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
+    private ApplicationContext applicationContext;
 
     @GetMapping("/greet")
     public String greet() {
-        return greetingService.sayHi();
+        return applicationContext.getBean(GreetingService.class).sayHi() + "|" + applicationContext.getBean(GreetingService.class).sayHi();
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -2,10 +2,12 @@ package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
+
 @Service
 public class GreetingService {
 
     String sayHi() {
-        return "hello world";
+        return "hello world" + LocalTime.now();
     }
 }
