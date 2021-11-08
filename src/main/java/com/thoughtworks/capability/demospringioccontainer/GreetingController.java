603353499e065327cea852a7326c4a24a1fc9dc2
LI Yan <li.yan@thoughworks.com>
2021-11-08 17:27:42 +0800

    [Li Yan] feat: finish problem 2 section 1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,11 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class GreetingController implements ApplicationContextAware {
 
     private final GreetingService greetingService;
 
@@ -19,4 +22,11 @@ public class GreetingController {
         return greetingService.sayHi();
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        var service1 = applicationContext.getBeansOfType(GreetingService.class);
+        var service2 = applicationContext.getBeansOfType(GreetingService.class);
+        System.out.println(service1);
+        System.out.println(service2);
+    }
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,11 +1,15 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class GreetingService {
 
     String sayHi() {
         return "hello world";
     }
+
 }
