97951b77d330b3c6408fa715b3bca1d0f9b0e606
LI Yan <li.yan@thoughworks.com>
2021-11-09 11:51:32 +0800

    [Li Yan] refactor: problem 3 solution 2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,26 +1,19 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GreetingService implements ApplicationContextAware {
+@RequiredArgsConstructor
+public class GreetingService {
 
-    private Foo foo;
-    private Bar bar;
+    private final Foo foo;
+    private final Bar bar;
 
     String sayHi() {
         foo.hi();
         bar.hi();
         return "hello world";
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        foo = applicationContext.getBean(Foo.class);
-        bar = applicationContext.getBean(Bar.class);
     }
 }
